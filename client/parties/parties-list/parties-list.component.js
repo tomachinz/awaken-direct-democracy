f8aeae5ae96ca7745a637b59fb7d32e012af03b9
Tom Atkinson <tom@funk.co.nz>
2016-03-12 19:22:40 +1300

    Finally working now
    :)

--- client/parties/parties-list/parties-list.component.js
+++ client/parties/parties-list/parties-list.component.js
@@ -24,7 +24,7 @@ class partiesList {
       name: 1
     };
     this.orderProperty = '1';
-    this.searchText = 'parties-list.component.js';
+    this.searchText = '';
 
     this.helpers({
       parties: this._partiesCollection,
--- model/users-list.js
+++ model/users-list.js
@@ -17,3 +17,28 @@ Meteor.methods({
   banana: function (partyId, userId) {},
   apples: function (partyId, rsvp) {}
 });
+
+
+Meteor.publish('thisNameDoesNotMatter', function () {
+  var self = this;
+  var handle = Meteor.users.find({}, {
+    fields: {emails: 1, profile: 1}
+  }).observeChanges({
+    added: function (id, fields) {
+      self.added('thisNameMatters', id, fields);
+    },
+    changed: function (id, fields) {
+      self.changed('thisNameMatters', id, fields);
+    },
+    removed: function (id) {
+      self.removed('thisNameMatters', id);
+    }
+  });
+
+  self.ready();
+
+  self.onStop(function () {
+    handle.stop();
+  });
+
+});

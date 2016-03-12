2beef435aa26988be1f5fa877d8245fc3cb18b1e
Tom Atkinson <tom@funk.co.nz>
2016-03-12 18:17:51 +1300

    First changes
    tried to exclude the api keys

--- client/parties/parties-list/parties-list.component.js
+++ client/parties/parties-list/parties-list.component.js
@@ -3,6 +3,7 @@ let {Component, View, SetModule, Inject, MeteorReactive, LocalInjectables} = ang
 SetModule('socially');
 
 @Component({selector: 'parties-list'})
+@Component({selector: 'users-list'})
 @View({
   templateUrl: () => {
     if (Meteor.isCordova) {
@@ -15,6 +16,62 @@ SetModule('socially');
 @Inject('$mdDialog', '$filter')
 @MeteorReactive
 @LocalInjectables
+
+class usersList {
+  constructor() {
+    this.perPage = 10;
+    this.page = 1;
+    this.sort = {
+      name: 1
+    };
+    this.orderProperty = '1';
+    this.searchText = 'usersList Class';
+
+    this.helpers({
+      parties: this._partiesCollection,
+      users: this._usersCollection,
+      isLoggedIn: this._isLoggedIn,
+      currentUserId: this._currentUserId,
+      images: this._imagesCollection
+    });
+    // this.subscribe('images');
+    this.subscribe('users');
+    // this.subscribe('parties', this._partiesSubscription.bind(this));
+    // this.defineMapProperties();
+  }
+  _imagesCollection() {
+    return Images.find({});
+  }
+  _currentUserId() {
+    return Meteor.userId();
+  }
+  _isLoggedIn() {
+    return Meteor.userId() !== null;
+  }
+  _usersCollection() {
+    return Meteor.users.find({});
+  }
+  updateSort() {
+    this.sort = {
+      name: parseInt(this.orderProperty)
+    }
+  }
+  getUserById(userId) {
+    return Meteor.users.findOne(userId);
+  }
+  getMainImage(images, onlyUrl) {
+    if (images && images.length && images[0] && images[0]) {
+      var url = this.$filter('filter')(this.images, {_id: images[0]})[0].url();
+      if (onlyUrl) {
+        return url;
+      }
+      return {
+        'background-image': 'url("' + url + '")'
+      }
+    }
+  }
+}
+
 class partiesList {
   constructor() {
     this.perPage = 3;
@@ -23,7 +80,7 @@ class partiesList {
       name: 1
     };
     this.orderProperty = '1';
-    this.searchText = '';
+    this.searchText = 'parties-list.component.js';
 
     this.helpers({
       parties: this._partiesCollection,
